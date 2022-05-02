decfd39235d9f4417a882c059c9f1c43ac7387fb
Krzysztof Żuraw <services@kzuraw.com>
2022-05-02 14:50:25 +0200

    fix: prefill

--- delivery-tracking-widget/vite.config.ts
+++ delivery-tracking-widget/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: { open: "?identifier=01G1NA1AR4H64K3XTCPWCQRVXS&contact=ingrid@ingrid.com" },
+  server: { open: "?identifier=&contact=email@ingrid.com" },
 });
